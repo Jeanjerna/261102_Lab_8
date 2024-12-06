22e949cebd9896904e0a8526133b893a7c60a2c8
Chaichan <chaichanchaisak2549@gmail.com>
2024-12-06 15:33:57 +0700

    Lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,47 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// string print(string name)
+// {
+//     cout << name;
+// }
+
+int main()
+{
+    long double age , height , bounty ;
+    string character ;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100) character = "Chopper";
+        else if(height <180) character = "Usopp";
+        else
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 1.1e9) character = "Zoro";
+            else character = "Sanji";
+        }
+    }
+    else
+    {
+        if(age <= 60)
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 500e6) character = "Jinbe";
+            else character = "Franky";
+        }
+        else character = "Brook";
+        
+    }
+    
+    cout << "Your character = " << character;
+    
+    return 0;
+}
